a65047f4a9c66e2e96c31a14ff133cfe4241a3ea
NEHA20-1992 <107953328+NEHA20-1992@users.noreply.github.com>
2023-02-06 10:16:25 +0530

    mmicommit

---
+++ api/seed/seeder.go
@@ -0,0 +1,72 @@
+
+package seed
+
+import (
+	"log"
+
+	"github.com/jinzhu/gorm"
+	"github.com/neha20-1992/fullstack/api/models"
+)
+
+var users = []models.User{
+	models.User{
+		Nickname: "Steven victor",
+		Email:    "steven@gmail.com",
+		Password: "password",
+	},
+	models.User{
+		Nickname: "Martin Luther",
+		Email:    "luther@gmail.com",
+		Password: "password",
+	},
+	models.User{
+		Nickname: "Siva Mathur",
+		Email:    "nihu.y.20@gmail.com",
+		Password: "hello123",
+	},
+}
+
+var posts = []models.Post{
+	models.Post{
+		Title:   "Title 1",
+		Content: "Hello world 1",
+	},
+	models.Post{
+		Title:   "Title 2",
+		Content: "Hello world 2",
+	},
+	models.Post{
+		Title:   "Title 3",
+		Content: "Hello world 3",
+	},
+}
+
+func Load(db *gorm.DB) {
+
+	err := db.Debug().DropTableIfExists(&models.Post{}, &models.User{}).Error
+	if err != nil {
+		log.Fatalf("cannot drop table: %v", err)
+	}
+	err = db.Debug().AutoMigrate(&models.User{}, &models.Post{}).Error
+	if err != nil {
+		log.Fatalf("cannot migrate table: %v", err)
+	}
+
+	err = db.Debug().Model(&models.Post{}).AddForeignKey("author_id", "users(id)", "cascade", "cascade").Error
+	if err != nil {
+		log.Fatalf("attaching foreign key error: %v", err)
+	}
+
+	for i, _ := range users {
+		err = db.Debug().Model(&models.User{}).Create(&users[i]).Error
+		if err != nil {
+			log.Fatalf("cannot seed users table: %v", err)
+		}
+		posts[i].AuthorID = users[i].ID
+
+		err = db.Debug().Model(&models.Post{}).Create(&posts[i]).Error
+		if err != nil {
+			log.Fatalf("cannot seed posts table: %v", err)
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/neha20-1992/fullstack/api"
+
+func main() {
+	api.Run()
+}
